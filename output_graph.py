9695b0ca4c2a4e9b198d9ba40e114b19b043e8fd
meltemoacar-web <meltemoacar@gmail.com>
2026-07-28 12:49:11 -0700

    Final code

--- output_graph.py
+++ output_graph.py
@@ -2,16 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+#This code just simply reads the file where the data was spit out into and then graphs it. It is a good visual representation of the center points over time, you can see how it changes as noise location, height, differ. 
 df = pd.read_csv("output.txt")
 
-plt.plot(df['0'],df['0.0'], marker='o')
+plt.plot(df.t,df.value, marker='o')
 
 plt.title('Center points over time')
 plt.xlabel('Time')
 plt.ylabel('Center point')
+plt.axhline(y=374.5, color='red', linestyle='dashed')
 plt.grid(True)
 
 plt.show()
 print(df)
 
-#Graph looks weird cause I gotta go back and fix some stuff in the voigt profile file, the range is being messy...
